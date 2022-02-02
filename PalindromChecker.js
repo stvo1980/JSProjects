b6bb8c06d148eacef1a6c5d996315f98d9acbb3a
Oleg Vechir <62355443+stvo1980@users.noreply.github.com>
2022-02-01 20:35:15 -0600

    Update PalindromChecker.js

--- PalindromChecker.js
+++ PalindromChecker.js
@@ -3,7 +3,8 @@ function palindrome(str) {
     let regex = / |\W|_/ig
     let arr = str.replace(regex, "").split("")
     let reversed = [...arr].reverse()
-    let res = true
+    let res = true;
+    // added semicolon
     for(let i = 0; i <= arr.length; i++) {
         if(arr[i] != reversed[i]) {
         res = false
